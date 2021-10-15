673e55318dfc16135eb9f9d546e080256df047fa
yatharth <yatharthverma070@gmail.com>
2021-10-15 20:12:35 +0530

    Improving file upload components

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,6 +1,7 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
-import { UploadIcon } from "@heroicons/react/outline";
+import { UploadIcon, DocumentIcon } from "@heroicons/react/outline";
+import { Progress } from "theme-ui";
 
 const baseStyle = {
   flex: 1,
@@ -16,6 +17,7 @@ const baseStyle = {
   color: "#3F3F3F",
   outline: "none",
   transition: "border .24s ease-in-out",
+  cursor: "pointer",
 };
 
 const activeStyle = {
@@ -31,7 +33,15 @@ const rejectStyle = {
 };
 
 function FileUpload() {
-  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone();
+  const [selectedFiles, setSelectedFiles] = useState([]);
+  const {
+    getRootProps,
+    acceptedFiles,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone();
 
   const style = useMemo(
     () => ({
@@ -43,13 +53,30 @@ function FileUpload() {
     [isDragActive, isDragReject, isDragAccept]
   );
 
+  useEffect(() => {
+    setSelectedFiles([...acceptedFiles]);
+  }, [acceptedFiles]);
+
   return (
-    <div className="container">
+    <div className="container flex flex-col">
       <div {...getRootProps({ style })}>
         <input {...getInputProps()} />
         <UploadIcon className="h-8 w-8 mb-2" />
         <p>Drag 'n' drop file here, or click to select file</p>
       </div>
+      {selectedFiles.length > 0 && (
+        <div className="flex w-full h-auto p-4 bg-white mt-4">
+          {selectedFiles.map((selFile) => (
+            <div className="flex space-x-2">
+              <DocumentIcon className="w-10 h-10 text-gray-400" />
+              <div className="flex flex-col w-full">
+                <span className="mb-2">{selFile.name}</span>
+                <Progress max={1} value={1 / 2} style={{ width: "100%" }} />
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/PersonalSpace.js
+++ src/components/PersonalSpace.js
@@ -3,7 +3,7 @@ import PersonalSpaceImg from "../images/PersonalSpace.svg";
 
 function PersonalSpace() {
   return (
-    <div className="bg-white border flex flex-col justify-center w-full px-20 py-6 mt-8">
+    <div className="bg-white border flex flex-col justify-center w-full px-20 py-6 mt-6">
       <img src={PersonalSpaceImg} className="h-80 w-80 mx-auto" />
       <span className="text-lg">My Personal Space</span>
     </div>
