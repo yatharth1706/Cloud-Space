34b8ddc406868418d95a92d9dcc4bd70c17cb803
yatharth <yatharthverma070@gmail.com>
2021-10-16 16:49:22 +0530

    Add AWS Amplify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { AmplifySignOut, withAuthenticator, AmplifySignIn } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -12,4 +17,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Auth } from "aws-amplify";
 
 function Navbar() {
+  const handleLogout = async () => {
+    await Auth.signOut();
+    window.location.reload();
+  };
+
   return (
     <div className="flex justify-between w-full h-16 items-center shadow py-2 px-20">
       <span className="text-xl font-semibold">Cloud Space</span>
-      <button className="bg-gray-800 w-28 text-white py-1 px-2 rounded">Logout</button>
+      <button className="bg-gray-800 w-28 text-white py-1 px-2 rounded" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 }
