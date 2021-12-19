3122d42bd43011cf4c7eaf74ca1618805ef5472e
yatharth <yatharthverma070@gmail.com>
2021-12-19 17:55:44 +0530

    Added svgs for builder

---
+++ src/components/svgConfig.js
@@ -0,0 +1,13 @@
+const svgs = [
+  "abstract-envelope.svg",
+  "abstract-timekeeper.svg",
+  "bermuda-circle.svg",
+  "bermuda-diamond.svg",
+  "bermuda-square.svg",
+  "bermuda-traingle.svg",
+  "bullseye-gradient.svg",
+  "colorful-stingrays.svg",
+  "confetti-doodles.svg",
+];
+
+export default svgs;
